b63b2beccad0f01669cff97b367eb5e7eec88606
alexis <alexis.fis02@gmail.com>
2023-07-30 23:37:17 -0300

    -Correcciones y etc

--- src/main/java/Javastral/com/gestorMateriasWeb/model/SubjectRepository.java
+++ src/main/java/Javastral/com/gestorMateriasWeb/model/SubjectRepository.java
@@ -8,8 +8,11 @@ import org.springframework.stereotype.Repository;
 import java.util.Set;
 
 @Repository
-public interface SubjectRepository extends JpaRepository <PassedSubject,PassedSubjectKey> {
+public interface SubjectRepository extends JpaRepository <Subject,String> {
 
-    @Query(value = "SELECT subject_id as subjectId,score FROM passed_subjects WHERE user_id = :id", nativeQuery = true)
+    @Query(value = "SELECT subject_id AS subjectId,score FROM passed_subjects WHERE user_id = :id", nativeQuery = true)
     Set<PassedSubjectScoreProjection> getPassedSubjectsScoreByUserId(@Param("id") String id);
+
+    @Query(value = "SELECT s.* FROM subjects AS s JOIN curriculums_subjects AS cs ON cs.subject_id = s.id WHERE curriculum_id = :id", nativeQuery = true)
+    Set<Subject> getCurriculumById(@Param("id") String id);
 }
