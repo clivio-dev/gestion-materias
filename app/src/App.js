63df931fc0bb5156e2be31405ef55a4770c259ff
Gonza <103864746+Gonzalo-e-Sosa@users.noreply.github.com>
2023-08-11 15:11:20 -0300

    Upload js files

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
 
-function App() {
+import React, { useState } from 'react';
+import Login from './components/Login';
+import CarreraSelector from './components/CarreraSelector';
+import MateriasTree from './components/MateriasTree';
+import data from './mocks/departamentos.json'; 
+
+const App = () => {
+  const [user, setUser] = useState(null);
+  const [selectedCarrera, setSelectedCarrera] = useState('');
+
+  const handleLogin = (userData) => {
+    // Aquí puedes implementar la lógica de verificación del inicio de sesión
+    // Por ahora, simplemente establecemos el usuario con los datos proporcionados
+    setUser(userData);
+  };
+
+  const handleSelectCarrera = (carreraCod) => {
+    setSelectedCarrera(carreraCod);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      {!user ? (
+        <Login onLogin={handleLogin} />
+      ) : (
+        <>
+          <h1>Bienvenido, {user.username}!</h1>
+          <CarreraSelector
+            carreras={data.departamentos}
+            onSelectCarrera={handleSelectCarrera}
+          />
+          {selectedCarrera && (
+            <MateriasTree
+              materias={data.departamentos
+                .find((depto) => depto.cod === selectedCarrera)
+                .planes[0].materias} // Solo mostramos las materias del primer plan de estudio
+            />
+          )}
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default App;
