2cd056a92667a9ea4878c7aeb7a208fdabfa5087
Gonza <gonzalososa342@gmail.com>
2023-12-28 22:07:50 -0300

    Router

---
+++ app/gestion-materias-app/src/main.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom"
+import ErrorPage from './error-page'
+import App from './App.tsx'
+import './index.css'
+import Login from './routes/login'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "login/",
+    element: <Login />
+  }
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
