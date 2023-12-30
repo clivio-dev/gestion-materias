14de2f3c353a9b724370b0dd510eaa9cc58be2f4
Gonza <gonzalososa342@gmail.com>
2023-12-30 20:34:21 -0300

    Ruta de login

--- app/gestion-materias-app/src/routes/login.tsx
+++ app/gestion-materias-app/src/routes/login.tsx
@@ -58,13 +58,6 @@ const Login = () => {
                 </button>
               </div>
             </form>
-  
-            <p className="mt-10 text-center text-sm text-gray-500">
-              Not a member?{' '}
-              <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                Start a 14 day free trial
-              </a>
-            </p>
           </div>
         </div>
       </>
