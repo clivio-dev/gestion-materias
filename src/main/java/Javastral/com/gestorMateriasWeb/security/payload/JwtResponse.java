bbb06db9e3da942c46f76b0cc90b5c96e76e808e
lclivio <lclivio@unlam.edu.ar>
2024-01-31 22:39:37 -0300

    Creacion de payloads para Requests y Responses de Auth.

--- src/main/java/Javastral/com/gestorMateriasWeb/security/payload/JwtResponse.java
+++ src/main/java/Javastral/com/gestorMateriasWeb/security/payload/JwtResponse.java
@@ -2,6 +2,13 @@ package Javastral.com.gestorMateriasWeb.security.payload;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class JwtResponse {
 	private String token;
 	private String type = "Bearer";
@@ -10,61 +17,9 @@ public class JwtResponse {
 	private String email;
 	private List<String> roles;
 
-	public JwtResponse(String accessToken, Long id, String username, String email, List<String> roles) {
-		this.token = accessToken;
-		this.id = id;
-		this.username = username;
-		this.email = email;
-		this.roles = roles;
-	}
-
 	public JwtResponse(String accessToken, String username, List<String> roles) {
 		this.token = accessToken;
 		this.username = username;
 		this.roles = roles;
-	}
-
-	public String getAccessToken() {
-		return token;
-	}
-
-	public void setAccessToken(String accessToken) {
-		this.token = accessToken;
-	}
-
-	public String getTokenType() {
-		return type;
-	}
-
-	public void setTokenType(String tokenType) {
-		this.type = tokenType;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public List<String> getRoles() {
-		return roles;
 	}
 }
--- src/main/java/Javastral/com/gestorMateriasWeb/security/payload/MessageResponse.java
+++ src/main/java/Javastral/com/gestorMateriasWeb/security/payload/MessageResponse.java
@@ -1,17 +1,12 @@
 package Javastral.com.gestorMateriasWeb.security.payload;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class MessageResponse {
 	private String message;
-
-	public MessageResponse(String message) {
-		this.message = message;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
 }
