2178ce09400917d3524b8adb08600a34289b6baa
lclivio <lclivio@unlam.edu.ar>
2024-01-31 22:41:19 -0300

    Comentados y pausados desarrollos del Controlador UserController para build de la app con Auth.

--- src/main/java/Javastral/com/gestorMateriasWeb/web/controller/UserController.java
+++ src/main/java/Javastral/com/gestorMateriasWeb/web/controller/UserController.java
@@ -1,16 +1,20 @@
 package Javastral.com.gestorMateriasWeb.web.controller;
 
-import Javastral.com.gestorMateriasWeb.model.entity.UserEntity;
-import Javastral.com.gestorMateriasWeb.web.controller.request.SubjectUpdateDTO;
-import Javastral.com.gestorMateriasWeb.model.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import Javastral.com.gestorMateriasWeb.model.entity.UserEntity;
+import Javastral.com.gestorMateriasWeb.model.repository.UserRepository;
+import Javastral.com.gestorMateriasWeb.web.controller.request.SubjectUpdateDTO;
 
 @RestController
 @RequestMapping("/user")
@@ -34,21 +38,21 @@ public class UserController {
             Map<Long,Integer> newPassedSubjects = data.getSave();
             List<Long> deletePassedSubjects = data.getDelete();
 
-            deletePassedSubjects.forEach(user.getPassedSubjects()::remove);
-            newPassedSubjects.forEach(user.getPassedSubjects()::put);
+            //deletePassedSubjects.forEach(user.getPassedSubjects()::remove);
+            //newPassedSubjects.forEach(user.getPassedSubjects()::put);
             userRepository.save(user);
 
             return ResponseEntity.ok("Update successful");
         }
         return ResponseEntity.badRequest().body("Update couldn't be performed (user id dont exist)");
     }
-
-    @GetMapping("/passed-subjects")
-    ResponseEntity<Map<Long,Integer>> getPassedSubjects(){
-        String username = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
-        Optional<UserEntity> user = userRepository.findByUsername(username);
-
-        return user.map(value -> ResponseEntity.ok(value.getPassedSubjects()))
-                .orElseGet( () -> ResponseEntity.notFound().build());
-    }
+//
+//	@GetMapping("/passed-subjects")
+//	ResponseEntity<Map<Long, Integer>> getPassedSubjects() {
+//		String username = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
+//		Optional<UserEntity> user = userRepository.findByUsername(username);
+//
+//		return user.map(value -> ResponseEntity.ok(value.getPassedSubjects()))
+//				.orElseGet(() -> ResponseEntity.notFound().build());
+//	}
 }
