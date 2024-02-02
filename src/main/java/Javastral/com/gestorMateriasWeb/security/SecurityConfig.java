40d92b02db139ca8d8045dd90e4bed06266dcf52
lclivio <lclivio@unlam.edu.ar>
2024-02-02 00:31:57 -0300

    Se remueve routing de api/test y su controlador para incluirlos posteriormente en JUnit.

--- src/main/java/Javastral/com/gestorMateriasWeb/security/SecurityConfig.java
+++ src/main/java/Javastral/com/gestorMateriasWeb/security/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers("/api/auth/**").permitAll()
-						.requestMatchers("/api/test/**").permitAll().anyRequest().authenticated()
+						.anyRequest().authenticated()
 						);
 
 		http.authenticationProvider(this.authenticationProvider());
