f597680ab8bf19b650f19a32b7bb8ab081036fe9
Gonza <gonzalososa342@gmail.com>
2024-03-16 21:25:52 -0300

    Home path

---
+++ src/routes/App.jsx
@@ -0,0 +1,20 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import '../styles/routes/App.css'
+import Header from '../sections/Header'
+import Footer from '../sections/Footer'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <section>
+        <h1>Inicio</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit mollitia assumenda nemo iste possimus beatae consequuntur totam corrupti dolorum adipisci alias et quis soluta iure inventore, perspiciatis atque dolorem molestias!</p>
+      </section>
+      <Footer />
+    </>
+  )
+}
+
+export default App
