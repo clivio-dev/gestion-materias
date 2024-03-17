2d12668a23e17d458f6ae87bb3c3ab5944983183
Gonza <gonzalososa342@gmail.com>
2024-03-16 21:27:14 -0300

    Sections files

---
+++ src/sections/Header.jsx
@@ -0,0 +1,14 @@
+import Navigation from "../components/Navigation"
+import Logo from '../components/Logo'
+
+const Header = () => {
+  return (
+    <>
+      <Navigation>
+        <Logo />
+      </Navigation>
+    </>
+  )
+}
+
+export default Header
