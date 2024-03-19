62cb4632e1929c499d5307fbf72ddbf7b1a92b42
Gonza <gonzalososa342@gmail.com>
2024-03-19 20:03:37 -0300

    Redirect when user is not auth

---
+++ src/components/RequireAuth.jsx
@@ -0,0 +1,16 @@
+import { useLocation, Navigate } from "react-router-dom";
+import useAuth from "./useAuth";
+
+
+const RequireAuth = ({ children }) => {
+  const auth = useAuth();
+  const location = useLocation();
+
+  if (!auth.user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+}
+
+export default RequireAuth
